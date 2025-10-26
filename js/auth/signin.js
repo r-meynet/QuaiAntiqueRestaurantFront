4c7fe48763e83123c2ffad48d1dac9f892322d24
r-meynet <remy.meynet@outlook.fr>
2025-10-26 20:03:05 +0100

    Ajout logique de connexion avec token

---
+++ js/auth/signin.js
@@ -0,0 +1,22 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+    // Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+
+    if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
+        // Il faudra récupérer le vrai token
+        const token = "lkjsscnvlsdbdfuqbzelfjqsldjfbjbqsdfq";
+        // Placer ce token en cookie
+        setToken(token);
+
+        
+        window.location.replace("/");
+    } else {
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
