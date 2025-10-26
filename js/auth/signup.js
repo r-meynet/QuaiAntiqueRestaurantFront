5b4c51cc3147e36040892a871bb4d732fb743fd8
r-meynet <remy.meynet@outlook.fr>
2025-10-26 19:19:06 +0100

    ajout validation mdp et confirmation mdp js

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -17,10 +17,40 @@ function validateForm() {
     const nomOK = validateRequired(inputNom);
     const prenomOK = validateRequired(inputPrenom);
     const mailOK = validateMail(inputMail);
-    if (nomOK && prenomOK && mailOK) {
+    const passwordOK = validatePassword(inputPassword);
+    const passwordConfirmOK = validateConfirmationPassword(inputPassword, inputValidationPassword);
+
+    if (nomOK && prenomOK && mailOK && passwordOK && passwordConfirmOK) {
         btnValidation.disabled = false;
     } else {
         btnValidation.disabled = true;
+    }
+}
+
+function validateConfirmationPassword(inputPwd, inputConfirmPwd) {
+    if (inputPwd.value == inputConfirmPwd.value) {
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    } else {
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input) {
+    // Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if (passwordUser.match(passwordRegex)) {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
     }
 }
 
