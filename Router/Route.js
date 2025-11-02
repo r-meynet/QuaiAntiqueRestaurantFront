26fbf27845974cfd4b1e2e3dd52261b87eede74e
r-meynet <remy.meynet@outlook.fr>
2025-11-02 22:58:31 +0100

    Ajout des roles dans système routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,9 @@
 export default class Route {
-    constructor(url, title, pathHTML, pathJS = "") {
+    constructor(url, title, pathHTML, authorize, pathJS = "") {
         this.url = url;
         this.title = title;
         this.pathHTML = pathHTML;
+        this.authorize = authorize;
         this.pathJS = pathJS;
     }
 }
