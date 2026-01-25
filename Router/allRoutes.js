b8a80e91c1f0fec5dea1a37bcb7b1d6200e54fc8
r-meynet <remy.meynet@outlook.fr>
2026-01-25 23:10:08 +0100

    Ajout sanitize html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,11 +3,26 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/galerie", "La galerie", "/pages/galerie.html", []),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", [], "js/galerie.js"),
+    new Route(
+        "/signin",
+        "Connexion",
+        "/pages/auth/signin.html",
+        ["disconnected"],
+        "/js/auth/signin.js",
+    ),
+    new Route(
+        "/signup",
+        "Inscription",
+        "/pages/auth/signup.html",
+        ["disconnected"],
+        "/js/auth/signup.js",
+    ),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [
+        "client",
+        "admin",
+    ]),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
     new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
 ];
--- js/script.js
+++ js/script.js
@@ -89,3 +89,9 @@ function showAndHideElementsForRoles() {
         }
     });
 }
+
+function sanitizeHtml(text) {
+    const tempHtml = document.createElement("div");
+    tempHtml.textContent = text;
+    return tempHtml.innerHTML;
+}
